b8a6ca1f04c79f5141ae21230474edcb796a46b7
hernan_jhc <hernan.jhc@gmail.com>
2019-04-09 21:08:04 -0300

    Se crean tablas por script, falta Productos que se creó por el Management

---
+++ sql/201904092031 - CreaProveedores.sql
@@ -0,0 +1,72 @@
+IF OBJECT_ID(N'[dbo].[FK_Proveedores_Domicilios]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Proveedores] DROP CONSTRAINT [FK_Proveedores_Domicilios];
+GO
+IF OBJECT_ID(N'[dbo].[FK_Proveedores_TiposDocumento]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Proveedores] DROP CONSTRAINT [FK_Proveedores_TiposDocumento];
+GO
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[Proveedores]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Proveedores];
+GO
+
+-- Creating table 'Proveedores'
+CREATE TABLE [dbo].[Proveedores] (
+    [Id] int  NOT NULL,
+    [RazonSocial] varchar(255)  NOT NULL,
+    [IdTipoDocumento] int  NOT NULL,
+    [NroDocumento] decimal(18,0)  NOT NULL,
+    [FechaNacimiento] datetime  NULL,
+    [EMail] varchar(255)  NULL,
+    [Direccion] varchar(255)  NULL,
+    [Sexo] char(1) NULL,
+	[IdDomicilio] int  NULL,
+    [Estado] tinyint  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Proveedores'
+ALTER TABLE [dbo].[Proveedores]
+ADD CONSTRAINT [PK_Proveedores]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [IdDomicilio] in table 'Proveedores'
+ALTER TABLE [dbo].[Proveedores]
+ADD CONSTRAINT [FK_Proveedores_Domicilios]
+    FOREIGN KEY ([IdDomicilio])
+    REFERENCES [dbo].[Domicilios]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_Proveedores_Domicilios'
+CREATE INDEX [IX_FK_Proveedores_Domicilios]
+ON [dbo].[Proveedores]
+    ([IdDomicilio]);
+GO
+
+-- Creating foreign key on [IdTipoDocumento] in table 'Proveedores'
+ALTER TABLE [dbo].[Proveedores]
+ADD CONSTRAINT [FK_Proveedores_TiposDocumento]
+    FOREIGN KEY ([IdTipoDocumento])
+    REFERENCES [dbo].[TiposDocumento]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_Proveedores_TiposDocumento'
+CREATE INDEX [IX_FK_Proveedores_TiposDocumento]
+ON [dbo].[Proveedores]
+    ([IdTipoDocumento]);
+GO
---
+++ sql/201904092054 - CreaUsuarios,Grupos.sql
@@ -0,0 +1,132 @@
+/****** Object:  Table [dbo].[Usuarios]    Script Date: 02/01/2019 11:39:59 a.m. ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Usuarios]') AND type in (N'U'))
+DROP TABLE [dbo].[Usuarios]
+GO
+/****** Object:  Table [dbo].[Carreras]    Script Date: 02/01/2019 11:39:59 a.m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Usuarios]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[Usuarios](
+	[Id] [int] NOT NULL,
+	[Nombre] [varchar](50) NOT NULL,
+	[Contraseña] [varchar](255) NOT NULL,
+	[FechaAlta] [datetime] NOT NULL,
+	[Estado] [tinyint] NOT NULL,
+	[FechaBaja] [datetime] NULL,
+	[NombreCompleto] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Usuarios] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_PADDING OFF
+GO
+ALTER AUTHORIZATION ON [dbo].[Usuarios] TO  SCHEMA OWNER 
+GO
+
+
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Grupos]') AND type in (N'U'))
+DROP TABLE [dbo].[Grupos]
+GO
+/****** Object:  Table [dbo].[Grupos]    Script Date: 07/01/2019 10:23:59 p.m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Grupos]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[Grupos](
+	[Id] [int] NOT NULL,
+	[Descripcion] [varchar](30) NOT NULL,
+	[Estado] [tinyint] NOT NULL,
+	CONSTRAINT [PK_Grupos] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_PADDING OFF
+GO
+ALTER AUTHORIZATION ON [dbo].[Grupos] TO  SCHEMA OWNER 
+GO
+
+
+
+
+
+/****** Object:  Table [dbo].[GruposUsuarios]    Script Date: 07/01/2019 23:09:59 p.m. ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GruposUsuarios]') AND type in (N'U'))
+DROP TABLE [dbo].[GruposUsuarios]
+GO
+/****** Object:  Table [dbo].[GruposUsuarios]    Script Date: 07/01/2019 23:09:59 p.m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GruposUsuarios]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[GruposUsuarios](
+	[Id] [int] NOT NULL,
+	[IdGrupo] [int] NOT NULL,
+	[IdUsuario] [int] NOT NULL,
+	CONSTRAINT [PK_GruposUsuarios] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_PADDING OFF
+GO
+ALTER AUTHORIZATION ON [dbo].[GruposUsuarios] TO  SCHEMA OWNER 
+GO
+
+
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [IdGrupo] in table 'GruposUsuarios'
+ALTER TABLE [dbo].[GruposUsuarios]
+ADD CONSTRAINT [FK_GruposUsuarios_Grupos]
+    FOREIGN KEY ([IdGrupo])
+    REFERENCES [dbo].[Grupos]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_GruposUsuarios_Grupos'
+CREATE INDEX [IX_FK_GruposUsuarios_Grupos]
+ON [dbo].[GruposUsuarios]
+    ([IdGrupo]);
+GO
+
+-- Creating foreign key on [IdUsuario] in table 'GruposUsuarios'
+ALTER TABLE [dbo].[GruposUsuarios]
+ADD CONSTRAINT [FK_GruposUsuarios_Usuarios]
+    FOREIGN KEY ([IdUsuario])
+    REFERENCES [dbo].[Usuarios]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_GruposUsuarios_Grupos'
+CREATE INDEX [IX_FK_GruposUsuarios_Usuarios]
+ON [dbo].[GruposUsuarios]
+    ([IdUsuario]);
+GO
